8e7b2458e8d3869b8f210ebbf7823fcd215affd4
HaruKasukabe <onizikoku@gmail.com>
2021-12-06 21:18:16 +0900

    Defineガード(言い方知らん)を追加しました

---
+++ DX11Game/Define.h
@@ -0,0 +1,58 @@
+//=============================================================================
+//
+// Define [Define.h]
+// Author : HARUO TAKEI
+//
+//=============================================================================
+#pragma once
+
+/* 使用方法
+   自分の名前のところを#ifndefから#ifdefに変更してください
+   他ソーファイルでは#ifndef 名前 で記述してください
+   #ifdef...有効 #ifndef...無効になります
+*/
+
+// 小楠裕子
+#ifndef OGUSU_YUKO
+#define OGUSU_YUKO
+#endif
+
+// 加藤匠
+#ifndef KATO_TAKUMI_
+#define KATO_TAKUMI_
+#endif
+
+// 武井遥都
+#ifndef TAKEI_HARUTO
+#define TAKEI_HARUTO
+#endif
+
+// 加藤将太
+#ifndef KATO_SHOTA
+#define KATO_SHOTA
+#endif
+
+// 管野佑斗
+#ifndef KANNO_YUTO
+#define KANNO_YUTO
+#endif
+
+// 藤田聖梨奈 
+#ifndef FUJITA_SERINA
+#define FUJITA_SERINA
+#endif
+
+// 中西健太
+#ifndef NAKANISHI_KENTA
+#define NAKANISHI_KENTA
+#endif
+
+// 大城セイキ
+#ifndef OSHIRO_SEIKI
+#define OSHIRO_SEIKI
+#endif
+
+// 日置陽介
+#ifndef HIOKI_KYOSUKE
+#define HIOKI_KYOSUKE
+#endif
