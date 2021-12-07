9cd7cf036d727b10376a5c8709e30d00c5934aea
TakumiKato <vanishu828@gmail.com>
2021-12-07 21:37:50 +0900

    木のオブジェクトのファイルを追加

---
+++ DX11Game/plant.h
@@ -0,0 +1,27 @@
+//=============================================================================
+//
+// êAï® [plant.h]
+// Author : â¡ì°Å@è†
+//
+//=============================================================================
+#pragma once
+
+#include "box.h"
+
+class Tree
+{
+public:
+	Tree();
+	~Tree();
+
+	void Update();
+
+	void OldDraw();
+	void NowDraw();
+
+	void Plant(XMFLOAT3);		// êAÇ¶ÇÈ
+	void Grow(XMFLOAT3);		// ê∂Ç¶ÇÈ
+
+private:
+	Box* m_pBox;
+};
